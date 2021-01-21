32f649246c11aebf66fb0c8657f6acdddeaa788b
DMalohlovets <dmytro.malohlovets@gmail.com>
2021-01-21 21:49:25 +0200

    done

--- src/test/java/dropit/shop/tests/BaseTest.java
+++ src/test/java/dropit/shop/tests/BaseTest.java
@@ -3,7 +3,11 @@ package dropit.shop.tests;
 import net.lightbody.bmp.BrowserMobProxy;
 import net.lightbody.bmp.BrowserMobProxyServer;
 import net.lightbody.bmp.client.ClientUtil;
-import net.lightbody.bmp.proxy.Whitelist;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorOutputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.brotli.dec.BrotliInputStream;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Proxy;
@@ -11,30 +15,91 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+//TODO replace System.out.println with log4j
+
 public class BaseTest {
+    final static String URL_TEMPLATE_TO_REPLACE = "/api/articles?limit=%s&offset=0";
+
     protected WebDriver driver;
     protected BrowserMobProxy proxy;
+
+    protected Boolean useProxy;
 
     @BeforeEach
     public void setUp() {
         proxy = new BrowserMobProxyServer();
         proxy.setTrustAllServers(true);
-        proxy.start(0);
+        proxy.start(8081);
         Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
 
-        // put our custom header to each request
         proxy.addRequestFilter((request, contents, messageInfo) -> {
-            // request.headers().add("my-test-header", "my-test-value");
-            System.out.println(request.headers().entries().toString());
+            String uri = request.getUri();
+
+            if (!useProxy || !uri.startsWith("/api/articles"))
+                return null;
+
+            System.out.println("PROXY ORIGINAL URI: " + uri);
+            String replaced = uri.replaceAll("limit=\\d+", "limit=12");
+            System.out.println("PROXY REPLACED URI: " + replaced);
+            request.setUri(replaced);
+
             return null;
         });
 
+        proxy.addResponseFilter((response, contents, messageInfo) -> {
+            String uri = messageInfo.getUrl();
 
+            if (!useProxy || !uri.contains("/api/articles"))
+                return;
+
+            System.out.println("PROXY RESPONSE URI: " + uri);
+
+            JSONObject json = null;
+            try (ByteArrayInputStream source = new ByteArrayInputStream(contents.getBinaryContents());
+                 BrotliInputStream brotliInputStream = new BrotliInputStream(source);
+                 InputStreamReader inputStreamReader = new InputStreamReader(brotliInputStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+            ) {
+                String contentText = bufferedReader
+                        .lines()
+                        .collect(Collectors.joining(System.lineSeparator()));
+
+                System.out.println("PROXY ORIGINAL RESPONSE: " + contentText);
+
+                json = new JSONObject(contentText);
+                json.remove("articlesCount");
+                json.put("articlesCount", 12);
+
+                System.out.println("PROXY REPLACED RESPONSE: " + json);
+
+            } catch (IOException e) {
+                System.out.println("Something went wrong with getting response body. Details:");
+                e.printStackTrace();
+            }
+
+            byte[] b = json.toString().getBytes(StandardCharsets.UTF_8);
+
+            try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+                bos.writeBytes(b);
+                CompressorOutputStream cos = new CompressorStreamFactory()
+                        .createCompressorOutputStream(CompressorStreamFactory.BROTLI, bos);
+                contents.setBinaryContents(bos.toByteArray());
+            } catch (CompressorException | IOException e) {
+                System.out.println("Something went wrong with setting response body. Details:");
+                e.printStackTrace();
+            }
+        });
 
         // Setting up Proxy for chrome
+        // TODO multi-browsers support
         ChromeOptions opts = new ChromeOptions();
-        String proxyOption = "--proxy-server=" + seleniumProxy.getHttpProxy();
-        opts.addArguments(proxyOption);
+        opts.setAcceptInsecureCerts(true);
+        opts.addArguments("--proxy-server=" + seleniumProxy.getHttpProxy());
+        opts.addArguments("--disable-web-security");
         //TODO Implement Win VS Linux support
         System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
         driver = new ChromeDriver(opts);
--- src/test/java/dropit/shop/tests/UISmokeTests.java
+++ src/test/java/dropit/shop/tests/UISmokeTests.java
@@ -3,22 +3,45 @@ package dropit.shop.tests;
 import dropit.shop.pages.HomePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.opentest4j.AssertionFailedError;
 
 public final class UISmokeTests extends BaseTest {
     @Test
-    public void testProxifying() {
+    public void testProxifying() throws InterruptedException {
         //TODO move URL to properties
-        proxy.newHar("google.com.ua");
-        driver.get("http://www.google.com.ua");
-        driver.get("http://localhost:4100/");
+        String uri = "http://localhost:4100/";
 
+        driver.get(uri);
         HomePage homePage = new HomePage(driver);
 
         homePage.waitPageLoaded();
         homePage.globalFeed.click();
+        homePage.waitPagination();
 
+        Assertions.assertAll(() -> {
+                    Assertions.assertEquals(10, homePage.articles.size(), "Unexpected articles on the page");
+                    Assertions.assertEquals(50, homePage.pages.size(), "Unexpected pages");
+                }
+        );
+        WebElement testTag = homePage.popularTags.stream()
+                .filter(t -> "test".equals(t.getText()))
+                .findFirst()
+                .orElseThrow(() -> new AssertionFailedError("No popular tag 'test' found"));
 
-        Assertions.assertEquals(driver.findElement(By.xpath("//body")).getText(), "{\"SUCCESS\"}", "unexpected page body on 'Your Feed' page");
+        testTag.click();
+
+        useProxy = true;
+        proxy.newHar(uri);
+        driver.get(uri);
+        HomePage page = new HomePage(driver);
+        page.waitPagination();
+
+        Assertions.assertAll(() -> {
+                    Assertions.assertEquals(10, page.articles.size(), "Unexpected articles on the page");
+                    Assertions.assertEquals(2, page.pages.size(), "Unexpected pages");
+                }
+        );
+        proxy.endHar();
     }
 }
